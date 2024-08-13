0fd0cca207f2ff7724abc05d403378537be8c0f5
Gaurav Prajapati <gauravprajapati2022@gmail.in>
2024-08-13 20:04:10 +0530

    Bg - changer mini project

---
+++ learning/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function App() {
+    const [color, setColor] = useState("olive")
+
+  return (
+    <div className='w-full h-screen duration-200'
+     style={{backgroundColor: color}}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          
+        <button 
+        onClick={() => setColor("pink")}
+        className='outline-none px-6 py-2 rounded-full text-white shadow-lg' 
+          style={{backgroundColor:"pink"}}
+          >pink</button>
+          <button
+          onClick={() => setColor("green")}
+           className='outline-none px-6 py-2 rounded-full text-white shadow-lg' 
+          style={{backgroundColor:"green"}}
+          >Green</button>
+          <button
+          onClick={() => setColor("blue")}
+           className='outline-none px-6 py-2 rounded-full text-white shadow-lg' 
+          style={{backgroundColor:"blue"}}
+          >Blue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
